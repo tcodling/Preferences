ef00b6927e0aa164c323fac316492edce765a810
tcodling <thomas.codling@outlook.com>
2020-03-27 10:39:54 +1300

    Setup index

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
+const data = require('../data.json')
+
 router.get('/', (req, res) => {
-    res.render('partials/index.hbs')
+    const viewData = {
+        people: data.people 
+    }
+    res.render('partials/index.hbs', viewData)
 })
 
 module.exports = router
