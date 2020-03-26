c9d954768ee2788fc27ad07aff2c37bef7cf7c7a
KimHartley <kimhartley1986@gmail.com>
2020-03-27 10:46:48 +1300

    profile initial

--- routes/profile.js
+++ routes/profile.js
@@ -2,8 +2,21 @@ const express = require('express')
 
 const router = express.Router()
 
+const data = require('../data.json')
+
 router.get('/', (req, res) => {
-    res.render('partials/profile.hbs')
+    const id = req.params.id
+    const people = data.people.find(item => item.id == id)
+
+    const viewData = {
+        name: people.name,
+        drink: people.drink,
+        sugar: people.sugar,
+        milk: people.milk
+    }
+
+    const template = 'views/partials/profile'
+    res.render('partials/profile.hbs', viewData)
 })
 
 module.exports = router
